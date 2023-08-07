011b5ee98a2cd7f2f1ecf641b2f845dd1df32a57
Dilshod Tadjie <d.tadjiev@gmail.com>
2023-08-07 11:43:04 +0500

    Learning Rust programming language with examples

---
+++ src/variables/compound/intro/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    println!("Compound type");
+    println!("Compound types can group multiple values into one type. Rust has two primitive compound types: tuples and arrays.")
+}
---
+++ src/variables/sclar/booleans/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    println!("Boolean type");
+
+    let t = true;
+    let f:bool = false;
+
+    println!("Boolean T : {}",t);
+    println!("Boolean F : {}",f);
+}
